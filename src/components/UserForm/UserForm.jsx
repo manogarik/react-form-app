6bf90070f401ce47cd9397cada3eb1799bc3c337
Manogari <manogarikumaraguru@gmail.com>
2025-12-16 14:20:57 +0530

    emailfield-added

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -5,26 +5,32 @@
 
     const [name, setName] = useState("");
     const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
  
  //On submission
  const handleSubmit = (e) =>{
     e.preventDefault();
-    alert(`Name : ${name}\n Phone : ${phone}`);
+    alert(`Name : ${name}\n Phone : ${phone}\n Email : ${email}`);
     //clearform
     setName("");
+    setPhone("");
+    setEmail("");
  };
  
  return(
     <form className="formstyle" onSubmit={handleSubmit}>
         <div>
-            
             <label>Name  : </label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name"/>
         </div>
         <div>
             <label>Phone : </label>
-            <input type="tel" value={phone} on onChange={(e) => setPhone(e.target.value)} placeholder="Enter your mobilenumber"/>
+            <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Enter your mobilenumber"/>
 
+        </div>
+        <div>
+            <label>Email Address : </label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your emailaddress"/>
         </div>
         <button type="submit">Submit</button>
     </form>
