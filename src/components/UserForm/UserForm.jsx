ec984d058efcb9d4e1c7cc30ed8ee27e3b41b248
Manogari <manogarikumaraguru@gmail.com>
2025-12-16 14:16:29 +0530

    Phonefield-added

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -4,20 +4,27 @@
  const UserForm = () =>{
 
     const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
  
  //On submission
  const handleSubmit = (e) =>{
     e.preventDefault();
-    alert(`Name : ${name}`);
+    alert(`Name : ${name}\n Phone : ${phone}`);
     //clearform
     setName("");
  };
  
  return(
     <form className="formstyle" onSubmit={handleSubmit}>
-        <div className="formstyle">
-            <label>Name : </label>
+        <div>
+            
+            <label>Name  : </label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name"/>
+        </div>
+        <div>
+            <label>Phone : </label>
+            <input type="tel" value={phone} on onChange={(e) => setPhone(e.target.value)} placeholder="Enter your mobilenumber"/>
+
         </div>
         <button type="submit">Submit</button>
     </form>
